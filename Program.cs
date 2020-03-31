c12ca4b7b14cf0c77bf08a66bf491fee4f941b81
adityapatel9619 <55842340+adityapatel9619@users.noreply.github.com>
2020-03-31 14:36:02 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Bike_Manufacturing_New
+{
+    public class Program
+    {
+        private string _model, _color, _year;
+        private int _modelno = 201600;
+        public Program(string model, string color, string year)
+        {
+            _model = model;
+            _color = color;
+            _year = year;
+        }
+        public string Model {
+            get { return _model; }
+        }
+        public string Color {
+            get { return _color; }
+        }
+        public string Year
+        {
+            get { return _year; }
+        }
+
+        public int Modelno
+        {
+            get { return _modelno = _modelno + 1; }
+        }
+
+    }
+}
---
+++ bike.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bike_Manufacturing_New
+{
+    class bike
+    {
+        static void Main(string[] args)
+        {
+            Program p = new Program("Royal Enfield","Grey","2016");
+            Console.WriteLine("Bike Manufacturing Program");
+            Console.WriteLine("Enter no of bikes to be manufactured:");
+            int n = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine($"\nBike:{i}\nModel No:{p.Modelno}\nModel:{p.Model}\nColor:{p.Color}\nYear:{p.Year}");
+            }
+        }
+    }
+}
